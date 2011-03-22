66c1e8e7239a5263758ef38718a99dd9e2fb8759
Stephen Ierodiaconou <stevegeek@gmail.com>
2011-03-22 17:12:34 +0200

    Add sub sampled to Ycbcr image method

---
+++ src/+Subsampling/supportedModes.m
@@ -0,0 +1,6 @@
+function [ modes ] = supportedModes( )
+%SUPPORTEDMODES Summary of this function goes here
+%   Detailed explanation goes here
+
+    modes = {'4:4:4','4:4:0','4:2:2','4:2:0','4:1:1','4:1:0'};
+end
