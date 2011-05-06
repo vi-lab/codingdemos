3cb86c82752eb4d8ecad5443f1c92d61f5e420db
Stephen Ierodiaconou <stevegeek@gmail.com>
2011-05-06 15:06:06 +0300

    Fix signed versions too

--- +Utilities/logicalArrayToSignedNumericArray.m
+++ +Utilities/logicalArrayToSignedNumericArray.m
@@ -6,17 +6,6 @@ if ~exist('padding', 'var')
     padding = 1;
 end
 
-len = length(binaryArray);
-array = [];
-
-for i=1:8:len
-    if i + 7 > len
-        lastBits = binaryArray(i:(i + (len - i)));
-        paddingBits = logical(ones(1, 7 - (len - i)) .* padding);
-        array = cat(2, array, Utilities.logicalToSignedDecimal([lastBits paddingBits]));
-    else
-        array = cat(2, array, Utilities.logicalToSignedDecimal(binaryArray(i:i + 7)));
-    end
-end
-
+paddedArray = Utilities.padLogicalArray(binaryArray, 8, padding);
+array = Utilities.logicalToSignedDecimal(reshape(paddedArray, 8, []).');
 end
