0382e9d2e84a25b008b19349a1813553dd715483
Stephen Ierodiaconou <stevegeek@gmail.com>
2011-05-06 22:17:59 +0300

    Make zerosRunLengthCoding take whole coeff block.

--- +TransformCoding/zerosRunLengthCoding.m
+++ +TransformCoding/zerosRunLengthCoding.m
@@ -1,54 +1,57 @@
-function rlcData = zerosRunLengthCoding(zigzag)
+function rlcDataBlock = zerosRunLengthCoding(coeffsBlock)
 
-% COMMENT ME UP
-
-% TODO: optimise away some for loops and maybe rethink this, ie not use
-% blkproc and use something else
+% input data is takes as 64 values raster order
+coeffs = reshape(coeffsBlock.', 64, []).'; % now 1 row per block
+rlcData = ones(size(coeffs,1), (size(coeffs,2)*2)-2);
 
 % We use -1 padding. A count of -1 means this pair is nothing and end of
-% data. We need the padding so output of blkproc is equal for each block
-% and thus can be joined into one output matrix
-%rlcdata = ones(1, 64);
-runLengths =    -ones(1, 63);
-values =        -ones(1, 63);
+% data. We need the padding so to avoid cell arrays
 
-ACvalues = zigzag(2:end);
-% RLE
-% find runs of zeros + magnitude , encode as value to lookup in table
-ind = find(ACvalues);
-numberOfValues = length(ind);
-% Run length is essentially the difference between the current nonzero
-% index and the previous one
-pind = [0 ind(1:end-1)];
-runLengths(1:numberOfValues) = ind - pind - 1;
-values(1:numberOfValues) = ACvalues(ind);
+% For each row
+for block=1:size(coeffs, 1)
+    runLengths =    -ones(1, 63);
+    values =        -ones(1, 63);
 
-if isempty(ind) || ~isempty(ind) && (ind(end) ~= length(ACvalues))
-    % there is a EOB
-    runLengths(numberOfValues+1)   = 0;
-    values(numberOfValues+1)       = 0;
-end
+    ACvalues = coeffs(block, 2:end);
+    % RLE
+    % find runs of zeros + magnitude , encode as value to lookup in table
+    ind = find(ACvalues);
+    numberOfValues = length(ind);
+    % Run length is essentially the difference between the current nonzero
+    % index and the previous one
+    pind = [0 ind(1:end-1)];
+    runLengths(1:numberOfValues) = ind - pind - 1;
+    values(1:numberOfValues) = ACvalues(ind);
 
-% Now must handle special JPEG standard case limiting zeros to 15 for run
-if nnz(runLengths > 15)
-    overFifteens = (runLengths > 15);
-    % Work out how many (15,0)s are needed
-    fifteensNeeded  = floor(runLengths(overFifteens) / 15);
-    remainders      = rem(runLengths(overFifteens), 15);
-    % Update with remainder zeros number
-    runLengths(overFifteens) = remainders;
-    % Insert (15,0)s
-    for i=1:length(fifteensNeeded)
-        if fifteensNeeded(i)
-            vs = ones(1,fifteensNeeded(i)).*15;
-            zs = zeros(1,fifteensNeeded(i));
-             % insert and remove last 2 values which will be -1s
-            runLengths = [runLengths(1:i-1) vs runLengths(i:end - 1)];
-            values = [values(1:i-1) zs values(i:end - 1)];
+    if isempty(ind) || ~isempty(ind) && (ind(end) ~= length(ACvalues))
+        % there is a EOB
+        runLengths(numberOfValues+1)   = 0;
+        values(numberOfValues+1)       = 0;
+    end
+
+    % Now must handle special JPEG standard case limiting zeros to 15 for run
+    if nnz(runLengths > 15)
+        overFifteens = (runLengths > 15);
+        % Work out how many (15,0)s are needed
+        fifteensNeeded  = floor(runLengths(overFifteens) / 15);
+        remainders      = rem(runLengths(overFifteens), 15);
+        % Update with remainder zeros number
+        runLengths(overFifteens) = remainders;
+        % Insert (15,0)s
+        for i=1:length(fifteensNeeded)
+            if fifteensNeeded(i)
+                vs = ones(1,fifteensNeeded(i)).*15;
+                zs = zeros(1,fifteensNeeded(i));
+                 % insert and remove last 2 values which will be -1s
+                runLengths = [runLengths(1:i-1) vs runLengths(i:end - 1)];
+                values = [values(1:i-1) zs values(i:end - 1)];
+            end
         end
     end
+
+    rlcData(block,:) = [runLengths values];
 end
 
-rlcData = [runLengths values];
+rlcDataBlock = reshape(rlcData.', [], size(coeffsBlock,1)).'; % now 1 row per block
 
-return
+end
