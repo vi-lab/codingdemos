6762e1cf3fbf11fc68a1717396b936420b0428c4
Stephen Ierodiaconou <stevegeek@gmail.com>
2011-05-06 18:29:31 +0300

    Vectorise zerosRunLengthCoding in its current state

--- +TransformCoding/zerosRunLengthCoding.m
+++ +TransformCoding/zerosRunLengthCoding.m
@@ -12,60 +12,43 @@ function rlcData = zerosRunLengthCoding(zigzag)
 runLengths =    -ones(1, 63);
 values =        -ones(1, 63);
 
-%DCvalue = zigzag(1);
 ACvalues = zigzag(2:end);
 % RLE
 % find runs of zeros + magnitude , encode as value to lookup in table
 ind = find(ACvalues);
+numberOfValues = length(ind);
+% Run length is essentially the difference between the current nonzero
+% index and the previous one
+pind = [0 ind(1:end-1)];
+runLengths(1:numberOfValues) = ind - pind - 1;
+values(1:numberOfValues) = ACvalues(ind);
 
-prev = 0;
-
-for i = 1:length(ind)
-    %RLCruns(i) = ind(i) - prev - 1;
-    %rlcdata((i*2)-1: i*2) = [(ind(i) - prev - 1) ACvalues(ind(i))];
-    runLengths(i)   = (ind(i) - prev - 1);
-    values(i)       = ACvalues(ind(i));
-    prev = ind(i);
-end
-if prev ~= length(ACvalues)
+if isempty(ind) || ~isempty(ind) && (ind(end) ~= length(ACvalues))
     % there is a EOB
-    % if there are any non-zero values i will be nonzero and we should
-    % incrememnt i to make EOB at end. If i = [] then there all block is
-    % zeros and should set a EOB at start
-    if (isempty(i))
-        i = 0;
-    end
-    i = i + 1;
-    %rlcdata((i*2)-1: i*2) = [0 0];
-    runLengths(i)   = 0;
-    values(i)       = 0;
+    runLengths(numberOfValues+1)   = 0;
+    values(numberOfValues+1)       = 0;
 end
-%RLCvalues = ACvalues(ind);
-
 
 % Now must handle special JPEG standard case limiting zeros to 15 for run
-for i=1:length(runLengths)
-    if runLengths(i) < 0
-        break;
-    end
-    if  runLengths(i) > 15 
-        % Work out how many (15,0)s are needed and insert
-        fifteenzs = floor(runLengths(i) / 15);
-        remz = rem(runLengths(i), 15);
-        % Update with remainder zeros number
-        runLengths(i) = remz;
-        % Insert (15,0)s
-        for k=1:fifteenzs
-            runLengths = [runLengths(1:i-1) 15 runLengths(i:end - 1)]; % insert and remove last 2 values which will be -1s
-            values = [values(1:i-1) 0 values(i:end - 1)]; % insert and remove last 2 values which will be -1s
+if nnz(runLengths > 15)
+    overFifteens = (runLengths > 15);
+    % Work out how many (15,0)s are needed
+    fifteensNeeded  = floor(runLengths(overFifteens) / 15);
+    remainders      = rem(runLengths(overFifteens), 15);
+    % Update with remainder zeros number
+    runLengths(overFifteens) = remainders;
+    % Insert (15,0)s
+    for i=1:length(fifteensNeeded)
+        if fifteensNeeded(i)
+            vs = ones(1,fifteensNeeded(i)).*15;
+            zs = zeros(1,fifteensNeeded(i));
+             % insert and remove last 2 values which will be -1s
+            runLengths = [runLengths(1:i-1) vs runLengths(i:end - 1)];
+            values = [values(1:i-1) zs values(i:end - 1)];
         end
     end
 end
-%zigzag
-%runLengths
-%values
 
 rlcData = [runLengths values];
-
 
 return
