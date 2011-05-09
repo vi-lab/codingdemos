c1c22bdaa47b64441ce5e530be6524b13cec48b8
Stephen Ierodiaconou <stevegeek@gmail.com>
2011-05-09 18:48:19 +0300

    Add start.m as per readme, for now just start GUIs but ideally add more informative start

---
+++ start.m
@@ -0,0 +1,9 @@
+function start( input_args )
+%START Summary of this function goes here
+%   Detailed explanation goes here
+
+% For now start the first GUI screen
+order = GUIs.order();
+GUIs.(order{1})();
+
+end
