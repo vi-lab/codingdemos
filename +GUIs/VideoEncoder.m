5ba6d0f0ee91e59d46a18ae6dffb21afd83bef03
Stephen Ierodiaconou <stevegeek@gmail.com>
2011-05-22 14:15:37 +0100

    Adding output frames to video screen.

--- +GUIs/VideoEncoder.m
+++ +GUIs/VideoEncoder.m
@@ -5,13 +5,19 @@ classdef VideoEncoder < GUIs.base
     properties
         videoEncoder
 
+        hRepeatCheckBox
+
         videoInputAxes
         videoOutputAxes
+        videoResidualAxes
+        videoReconstructedResidualAxes
+        videoStatAxes
         videoMVAxes
 
         videoPlayers
         playTimer
-        repeatVideo = true;
+        repeatVideo = false;
+        frameStatsToPlot = [];
     end
 
     methods
@@ -30,35 +36,85 @@ classdef VideoEncoder < GUIs.base
                                                     'Separator','on', ...
                                                     'ClickedCallback', @(source, event)(obj.reloadVideo(source)));
 
-            obj.videoInputAxes  = obj.createAxesForImage([0.01 0.65 0.3 0.3], obj.hExternalPanel);
-            obj.videoOutputAxes = obj.createAxesForImage([0.65 0.65 0.3 0.3], obj.hExternalPanel);
-            obj.videoMVAxes     = obj.createAxesForImage([0.65 0.1 0.3 0.3], obj.hExternalPanel);
+            % draw encoder
+            %mainCanvas  = obj.createAxesForImage([0 0 1 1], obj.hExternalPanel);
+            mainCanvas = axes('Parent', obj.hExternalPanel, ...
+                                        'Box', 'off', ...
+                                        'Visible', 'on', ...
+                                        'XTick', [],...
+                                        'YTick', [],...
+                                        'XColor', [1 1 1],...
+                                        'YColor', [1 1 1],...
+                                        'Units', 'Normalized', ...
+                                        'Position', [0 0 1 1]);
+            rectangle('Position', [0.32 0.75 0.02 0.02], 'Curvature', [1,1], 'Parent', mainCanvas);
+            rectangle('Position', [0.35 0.70 0.10 0.05], 'Parent', mainCanvas);
+            rectangle('Position', [0.50 0.70 0.10 0.05], 'Parent', mainCanvas);
+            rectangle('Position', [0.50 0.50 0.10 0.05], 'Parent', mainCanvas);
+            rectangle('Position', [0.50 0.45 0.02 0.02], 'Curvature', [1,1], 'Parent', mainCanvas);
+            rectangle('Position', [0.50 0.40 0.10 0.05], 'Parent', mainCanvas);
+            rectangle('Position', [0.35 0.40 0.10 0.05], 'Parent', mainCanvas);
+            xlim(mainCanvas, [0 1]);
+            ylim(mainCanvas, [0 1]);
 
-            %obj.videoEncoder.playVideo('in', 'parent', obj.videoInputAxes, 'framerate', 5);
-            %obj.videoEncoder.playVideo('out', 'parent', obj.videoOutputAxes, 'framerate', 5);
-            %obj.videoEncoder.playVideo('out', 'parent', obj.videoMVAxes, 'framerate', 5, 'showMotionVectors', true, 'showResidual', true);
-            obj.setVideoPlayer(1, obj.videoEncoder.imageMatrix, obj.videoInputAxes);
-            obj.setVideoPlayer(2, obj.videoEncoder.reconstructedVideo, obj.videoOutputAxes);
+            obj.hRepeatCheckBox = uicontrol('Style', 'checkbox', ...
+                                    'Parent', obj.hExternalPanel, ...
+                                    'FontSize', 9, ...
+                                    'FontName', 'Courier New',...
+                                    'Units', 'Normalized', ...
+                                    'Position',[0.02 0.97 0.15 0.03],...
+                                    'String', 'Loop Video?',...
+                                    'Value', 0,...
+                                    'Callback', @(source, event)(obj.toggleLoopVideo(source)));
+
+            obj.videoInputAxes  = obj.createAxesForImage([0.01 0.65 0.3 0.3], obj.hExternalPanel);
+            obj.videoResidualAxes = obj.createAxesForImage([0.4 0.75 0.2 0.2], obj.hExternalPanel);
+            obj.videoReconstructedResidualAxes = obj.createAxesForImage([0.4 0.45 0.2 0.2], obj.hExternalPanel);
+            obj.videoOutputAxes = obj.createAxesForImage([0.65 0.65 0.3 0.3], obj.hExternalPanel);
+            obj.videoStatAxes   = axes('Position', [0.65 0.35 0.3 0.3], 'Parent', obj.hExternalPanel);
+            obj.videoMVAxes     = obj.createAxesForImage([0.65 0.01 0.3 0.3], obj.hExternalPanel);
+
+            obj.setVideoPlayer(1, 'input', obj.videoEncoder.imageMatrix, obj.videoInputAxes);
+            obj.setVideoPlayer(2, 'output', obj.videoEncoder.reconstructedVideo, obj.videoOutputAxes);
+            obj.setVideoPlayer(3, 'residual', obj.videoEncoder.predictionErrorFrame, obj.videoResidualAxes);
+            obj.setVideoPlayer(4, 'reconstructedresidual', obj.videoEncoder.reconstructedPredictionErrorFrame, obj.videoReconstructedResidualAxes);
             %obj.setVideoPlayer(3, cell2mat(obj.videoEncoder.reconstructedPredictionErrorFrame), obj.videoMVAxes);
             obj.playVideos();
         end
  
-        function setVideoPlayer(obj, playerID, matrix, axes)
-            obj.videoPlayers{playerID}.data = matrix;
-            obj.videoPlayers{playerID}.frame = 1;
+        function setVideoPlayer(obj, playerID, type, matrix, axes)
+            switch type
+                case {'residual', 'reconstructedresidual'}
+                    % turn into matrix block
+                    matrix = matrix.';
+                    m = zeros(size(obj.videoEncoder.imageMatrix, 1), size(obj.videoEncoder.imageMatrix, 2), 3, length(matrix(:)));
+                    for i=1:length(matrix(:))
+                        if ~isempty(matrix{i})
+                            if strcmp(type, 'reconstructedresidual')
+                                m(:,:,:,i) = Subsampling.subsampledToYCbCrImage(matrix{i});
+                            else
+                                m(:,:,:,i) = matrix{i};
+                            end
+                        end
+                    end
+                    obj.videoPlayers{playerID}.data = m;
+                    obj.videoPlayers{playerID}.type = 'residualmatrixdata';
+                case {'input','output'}  
+                    obj.videoPlayers{playerID}.data = matrix;
+                    obj.videoPlayers{playerID}.type = 'rgbimagedata';
+            end
+            obj.videoPlayers{playerID}.frame = 0;
             obj.videoPlayers{playerID}.parent = axes;
-            imshow(ycbcr2rgb(matrix(:,:,:,1)), 'Parent', axes);
         end
 
         function playVideos(obj)
             % update frame by frame, on a timer callback?
             if isempty(obj.playTimer)
-                obj.playTimer = timer('Period', 1.0/obj.videoEncoder.frameRate,'ExecutionMode', 'fixedrate', 'TimerFcn', @(x,y)obj.showNextFrame());
+                obj.playTimer = timer('Period', 1.0/obj.videoEncoder.frameRate,'ExecutionMode', 'fixedspacing', 'TimerFcn', @(x,y)obj.showNextFrame());
                 start(obj.playTimer);
             else
 
             end
-            
         end
 
         function showNextFrame(obj)
@@ -67,31 +123,57 @@ classdef VideoEncoder < GUIs.base
                 if obj.videoPlayers{i}.frame > size(obj.videoPlayers{i}.data, 4)
                     if obj.repeatVideo
                         obj.videoPlayers{i}.frame = 1;
+                        obj.frameStatsToPlot = [];
                     else
-                        stop(obj.playTimer);
-                        delete(obj.playTimer);
-                        obj.playTimer = [];
+                        obj.clearTimer();
+                        obj.frameStatsToPlot = [];
                         return;
                     end
                 end
-                imshow(ycbcr2rgb(obj.videoPlayers{i}.data(:,:,:,obj.videoPlayers{i}.frame)), 'Parent', obj.videoPlayers{i}.parent);
+                frame = obj.videoPlayers{i}.data(:,:,:,obj.videoPlayers{i}.frame);
+                switch obj.videoPlayers{i}.type
+                    case 'residualmatrixdata'
+                        imagesc(sum(frame,3), 'Parent', obj.videoPlayers{i}.parent);
+                        colormap bone
+                    case 'rgbimagedata'
+                        imshow(ycbcr2rgb(frame), 'Parent', obj.videoPlayers{i}.parent);
+                end
+
+                % update stats graph
+                if i == 1
+                    obj.frameStatsToPlot = [obj.frameStatsToPlot obj.videoEncoder.frameStatistics{obj.videoPlayers{i}.frame}.psnr];
+                    plot(obj.videoStatAxes, obj.frameStatsToPlot);
+                    xlim(obj.videoStatAxes, [1 size(obj.videoEncoder.imageMatrix, 4)]);
+                    %ylim(obj.videoStatAxes, [10 50]);
+                end
             end
         end
 
         function handleCloseRequest(obj, source, event)
+            obj.clearTimer();
+            delete(gcf);
+        end
+
+        function clearTimer(obj)
             if ~isempty(obj.playTimer)
                 stop(obj.playTimer);
                 delete(obj.playTimer);
                 obj.playTimer = [];
             end
-            delete(gcf);
         end
 
         function reloadVideo(obj, source)
+            obj.clearTimer();
             obj.videoEncoder = Video.encoder('examples/imseq/vid:0000:0024:.jpg');
             obj.videoEncoder.encode('gop', 'ipppp', 'verbose', true, 'Quality', 80, 'BlockMatchingSearchDistance', 8, 'BlockMatching', 'DSA');
             ve = obj.videoEncoder;
             save('cachedVideo.mat', 've');
+        end
+
+        function toggleLoopVideo(obj,source)
+            obj.clearTimer();
+            obj.repeatVideo = ~obj.repeatVideo;
+            obj.playVideos();
         end
     end
 end 
