9936a19d31f17ded0eda027de89d2adc4d98d268
Stephen Ierodiaconou <stevegeek@gmail.com>
2011-05-28 17:21:18 +0100

    Added labels to Correlation GUI

--- +GUIs/Correlation.m
+++ +GUIs/Correlation.m
@@ -6,13 +6,18 @@ classdef Correlation < GUIs.base
         hMotionCorrelationPanel
         hImageAxes
         hSpatialCorrelationAxes
+        hSpatialInfo
+
         hImage
         chosenRow
         hChosenLine
+
         hVideoSelect
         hTemporalCorrelationAxes
         hVideoAxes
         hVideo
+        hVideoInfo
+
         videoEncoder
         chosenVideoPoint
     end
@@ -26,27 +31,35 @@ classdef Correlation < GUIs.base
 
             % Show input image selection
             obj.createInputImageSelectComboBoxAndText([0.06 0.96 0.25 0.03], [0.06 0.9 0.2 0.08]);
-            obj.hImageAxes = obj.createAxesForImage([0.01 0.5 0.3 0.4], obj.hExternalPanel);
-            obj.hSpatialCorrelationAxes = axes('Parent', obj.hExternalPanel, 'Position', [0.35 0.53 0.64 0.47]);
+            obj.hImageAxes = obj.createAxesForImage([0.01 0.55 0.3 0.35], obj.hExternalPanel);
+            obj.createTextElement([0.01 0.5 0.4 0.03], 'Click to select a row for correlation plot.', 9, 'on', 'white', obj.hExternalPanel);
+            obj.hSpatialCorrelationAxes = axes('Parent', obj.hExternalPanel, 'Position', [0.4 0.56 0.54 0.38]);
+            set(get(obj.hSpatialCorrelationAxes,'XLabel'),'String','Offset in Pixels');
+            set(get(obj.hSpatialCorrelationAxes,'YLabel'),'String','Correlation');
+            obj.hSpatialInfo = obj.createTextElement([0.35 0.97 0.4 0.03], ['Chosen Row: ' num2str(obj.chosenRow)], 12, 'on', 'white', obj.hExternalPanel);
+
             obj.hMotionCorrelationPanel = uipanel('FontSize', 12,  ...
                             'FontName', 'Courier', ...
                             'BackgroundColor', 'white', ...
                             'Units', 'Normalized', ...
                             'Position', [0.0 .01 1.0 .47]);
-
             obj.createTextElement([0.0 .93 1.0 0.07], ' Temporal Correlation', 15, true, [.8 .8 .8], obj.hMotionCorrelationPanel);
-            obj.hTemporalCorrelationAxes = axes('Parent', obj.hMotionCorrelationPanel, 'Position', [0.35 0.1 0.64 0.8]);
-            obj.hVideoAxes = obj.createAxesForImage([0.01 0.1 0.3 0.8], obj.hMotionCorrelationPanel);
+            obj.hTemporalCorrelationAxes = axes('Parent', obj.hMotionCorrelationPanel, 'Position', [0.4 0.15 0.54 0.65]);
+            set(get(obj.hTemporalCorrelationAxes,'XLabel'),'String','Offset in Time (Frames)');
+            set(get(obj.hTemporalCorrelationAxes,'YLabel'),'String','Correlation');
+            obj.hVideoInfo = obj.createTextElement([0.35 0.85 0.4 0.07], ['Chosen Pixel: (' num2str(obj.chosenVideoPoint(1)) ',' num2str(obj.chosenVideoPoint(2)) ')'], 12, 'on', 'white', obj.hMotionCorrelationPanel);
+            obj.hVideoAxes = obj.createAxesForImage([0.01 0.1 0.3 0.71], obj.hMotionCorrelationPanel);
             obj.hVideoSelect = uicontrol('Style', 'pushbutton', 'String', 'Load Video',...
                                         'Parent', obj.hMotionCorrelationPanel,...
                                         'Units', 'Normalized', ...
-                                        'Position', [0.01 0.01 0.15 0.07],...
+                                        'Position', [0.01 0.82 0.15 0.07],...
                                         'Callback', @(src,evt)obj.videoLoad);
             obj.hVideoSelect = uicontrol('Style', 'pushbutton', 'String', 'Play',...
                                         'Parent', obj.hMotionCorrelationPanel,...
                                         'Units', 'Normalized', ...
-                                        'Position', [0.16 0.01 0.15 0.07],...
+                                        'Position', [0.16 0.82 0.15 0.07],...
                                         'Callback', @(src,evt)obj.videoPlay);
+            obj.createTextElement([0.01 0.01 0.4 0.06], 'Click to select a pixel for the correlation plot.', 9, 'on', 'white', obj.hMotionCorrelationPanel);    
         end
 
         function videoLoad(obj)
@@ -65,16 +78,24 @@ classdef Correlation < GUIs.base
             % Y channel
             if ~isempty(obj.hChosenLine)
                 delete(obj.hChosenLine);
+                obj.hChosenLine = [];
             end
             data = obj.inputMatrix(:,:,1);
             coefs = xcorr(data(obj.chosenRow,:,1), size(data,2));
             plot(obj.hSpatialCorrelationAxes, coefs(ceil(length(coefs)/2):end));
+            set(obj.hSpatialInfo, 'String', ['Chosen Row: ' num2str(obj.chosenRow)]);
+            set(get(obj.hSpatialCorrelationAxes,'XLabel'),'String','Offset in Pixels');
+            set(get(obj.hSpatialCorrelationAxes,'YLabel'),'String','Correlation');
             obj.hChosenLine = rectangle('Parent', obj.hImageAxes, ...
                                        'Position', [0.5  obj.chosenRow-0.5 size(obj.inputMatrix,2) 1], ...
                                        'EdgeColor', [0 0 0], 'LineWidth', 1);
         end
 
         function updateAxes(obj)
+            if ~isempty(obj.hChosenLine)
+                delete(obj.hChosenLine);
+                obj.hChosenLine = [];
+            end
             obj.hImage = imshow(obj.inputMatrix(:,:,1), 'Parent', obj.hImageAxes);
             set(obj.hImage, 'ButtonDownFcn', @(source, evt)(obj.imageClick(source)));
         end
@@ -105,6 +126,9 @@ classdef Correlation < GUIs.base
                 coefs = xcorr(data, i);
                 %plot(obj.hTemporalCorrelationAxes, coefs(ceil(length(coefs)/2):end));
                 plot(obj.hTemporalCorrelationAxes, coefs(ceil(length(coefs)/2):end));
+                set(get(obj.hTemporalCorrelationAxes,'XLabel'),'String','Offset in Time (Frames)');
+                set(get(obj.hTemporalCorrelationAxes,'YLabel'),'String','Correlation');
+                set(obj.hVideoInfo, 'String', ['Chosen Pixel: (' num2str(obj.chosenVideoPoint(1)) ',' num2str(obj.chosenVideoPoint(2)) ')']);
                 pause(0.3);
             end 
         end
